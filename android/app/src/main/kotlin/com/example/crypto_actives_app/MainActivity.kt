49cb03137329e490f3024cff3c7e1767f9acf81f
Guillermo <guille54555@gmail.com>
2022-07-05 20:47:05 +0200

    Create a empty Project

---
+++ android/app/src/main/kotlin/com/example/crypto_actives_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crypto_actives_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CryptoActivesApp());
+}
+
+class CryptoActivesApp extends StatelessWidget {
+  const CryptoActivesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
